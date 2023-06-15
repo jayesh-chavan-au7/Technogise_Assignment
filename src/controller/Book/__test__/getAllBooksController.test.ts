cd70123f567752d50e35cb8c316d9fae6cce96b2
jayDan21 <jayesh.chavan3@dentsu.com>
2023-06-15 12:41:32 +0530

    feat: UTC

---
+++ src/controller/Book/__test__/getAllBooksController.test.ts
@@ -0,0 +1,11 @@
+// write down the test cases for getAllBooks function in controller, by mocking the service.
+import { describe } from '@jest/globals';
+describe('getAllBooks controller', () => {
+  it.skip('should return books without any filter', async () => {});
+  it.skip('should return books with filter, limit and skip', async () => {});
+  it.skip('should return empty array of data if no book found', async () => {});
+  it.skip('should throw error if getAllBooksDTO is invalid with filter', async () => {});
+  it.skip('should throw error if getAllBooksDTO is invalid with limit', async () => {});
+  it.skip('should throw error if getAllBooksDTO is invalid with skip', async () => {});
+  it.skip('should throw error if service throws error', async () => {});
+});
---
+++ src/service/BookService/__test__/getAllBooksService.test.ts
@@ -0,0 +1,9 @@
+describe('getAllBooks service', () => {
+  it.skip('should return books without any filter', async () => {});
+  it.skip('should return books with filter, limit and skip', async () => {});
+  it.skip('should filter books by title', async () => {});
+  it.skip('should filter books by author', async () => {});
+  it.skip('should filter books by isAvailable', async () => {});
+  it.skip('should filter books by category', async () => {});
+  it.skip('should throw error if repo throws error', async () => {});
+});
